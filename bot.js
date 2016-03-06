3f44f232c285992f1b8ae5bd67abff7e41b73510
Lucas Cavalheiro <lucasad.cavalheiro@gmail.com>
2016-03-05 22:26:22 -0300

    add index files

--- bot.js
+++ bot.js
@@ -1,3 +1,25 @@
+var token = '179409711:AAFk9y6pmOPAQHeo38ytIUmt-UHc5asU6_U';
 
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
 
 console.log('bot server started...');
+
+// hello command
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+// sum command
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+  var result = 0;
+  match[1].trim().split(/\s+/).forEach(function (i) {
+    result += (+i || 0);
+  })
+  bot.sendMessage(msg.chat.id, result).then(function () {
+    // reply sent!
+  });
+});
